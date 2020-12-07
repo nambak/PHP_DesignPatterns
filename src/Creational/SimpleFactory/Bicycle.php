7bda5b06c943de0dce30ce3fd7684e72e26d62ca
남성룡 <nambak80@gmail.com>
2020-12-07 17:16:24 +0900

    added SimpleFactory Pattern

---
+++ src/Creational/SimpleFactory/Bicycle.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types = 1);
+
+namespace DesignPatterns\Creational\SimpleFactory;
+
+class Bicycle
+{
+    public function driveTo(string $destination)
+    {
+        //
+    }
+}
