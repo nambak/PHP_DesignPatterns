4541991b1f9ed79f3ccd8806fcf1b7286bfaad83
남성룡 <nambak80@gmail.com>
2020-12-07 16:50:56 +0900

    added Pool Pattern

---
+++ Tests/PoolTest.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types = 1);
+
+namespace Tests;
+
+use DesignPatterns\Creational\Pool\WorkerPool;
+use PHPUnit\Framework\TestCase;
+
+class PoolTest extends TestCase
+{
+    public function testCanGetNewInstancesWithGet()
+    {
+        $pool = new WorkerPool();
+        $worker1 = $pool->get();
+        $worker2 = $pool->get();
+
+        $this->assertCount(2, $pool);
+    }
+}
